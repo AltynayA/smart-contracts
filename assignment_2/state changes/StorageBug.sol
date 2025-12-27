82805d82b67bbfe95286d67098878f5080329385
altynay <goldmoon321123@gmail.com>
2025-12-28 00:21:43 +0500

    Add a function to copy and modify from storage

--- assignment_2/state changes/StorageBug.sol
+++ assignment_2/state changes/StorageBug.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 // Create a function where modifying a storage variable inside memory copy leads to unexpected
 // behavior.
 contract StorageBug {
@@ -6,12 +7,23 @@ contract StorageBug {
     uint256[3] public importantNumbers = [10, 20, 35];
 
     // flawed function 
-    function dangerousCopyAndModify() public {
+    // function dangerousCopyAndModify() public {
         //  attempts to copy storage array into memory
-        uint256[] memory numbers = importantNumbers;
+        // uint256[] memory numbers = importantNumbers;
         // modifies the storage, not the memory
-        numbers[1] = 999;
+        // numbers[1] = 999;
+    // }
+
+    function safeCopyAndModifyFromStorage() public view returns (uint256[] memory) {
+        uint256[] memory numbers = new uint256[](importantNumbers.length);
+        
+        for (uint i = 0; i < importantNumbers.length; i++) {
+            numbers[i] = importantNumbers[i];
     }
+
+        numbers[1] = 999; // safe
+        return numbers;
+}
 
     function getNumber(uint256 index) external view returns (uint256) {
         return importantNumbers[index];
